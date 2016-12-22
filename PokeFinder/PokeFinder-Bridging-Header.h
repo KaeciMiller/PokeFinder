10fbb3670877afe04c1a24adf7e6d0db711038a8
Ashcott Monrovia <admin@admins-MacBook-Pro.local>
2016-12-22 07:23:18 -0600

    First commit

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Ashcott Monrovia, Inc. on 12/20/16.
+//  Copyright © 2016 Ashcott Monrovia, Inc. All rights reserved.
+//
+
+#import "GeoFire.h"
